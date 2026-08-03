e4eeb4fe0dae4770f144388690e1d4b9e76e6fe6
montmorill <montmorill@outlook.com>
2026-08-03 19:48:05 +0800

    refactor(markdown): enhance markdown processing with PUA validation and improved error handling

--- packages/element/markdown.ts
+++ packages/element/markdown.ts
@@ -1,18 +1,33 @@
 import type { Node, NodeType } from 'commonmark'
 import type { Fragment } from './jsx-runtime'
 import { Parser } from 'commonmark'
-import h, { Element } from './jsx-runtime'
+import h from './jsx-runtime'
 import { stripNulls } from './strip-nulls'
-import { pack } from './utils'
+import { replace, transform } from './utils'
 
-let slotValues: Fragment[] = []
+const PUA_START = 0xE000
+const PUA_SIZE = 6400
+const PUA_PATTERN = /[\uE000-\uF8FF]/u
 
 export function markdown(strings: TemplateStringsArray, ...values: Fragment[]): Fragment {
-  slotValues = values
-  const markdownText = strings.reduce((res, str, i) =>
-    res + str + (i < slotValues.length ? `<slot>${i}</slot>` : ''), '')
+  if (values.length > PUA_SIZE)
+    throw new Error(`values length must be less than ${PUA_SIZE}`)
+  const markdownText = strings.reduce((res, str, index) => {
+    if (PUA_PATTERN.test(str))
+      throw new Error(`strings must not contain PUA characters: ${str}`)
+    res += str
+    if (index < values.length)
+      res += String.fromCodePoint(PUA_START + index)
+    return res
+  }, '')
   const ast = new Parser().parse(markdownText)
-  return transformNode(ast)
+  const fragment = transformNode(ast)
+  return transform(fragment, {
+    text: text => replace(text, PUA_PATTERN, (match) => {
+      const codepoint = match.codePointAt(0)
+      return values[codepoint! - PUA_START]!
+    }),
+  })
 }
 
 export interface MarkdownElement {
@@ -38,23 +53,23 @@ declare global {
 }
 
 const TRANSFORMERS: Record<NodeType, (node: Node) => Fragment> = {
-  text: node => esacpeSlot(node.literal!),
+  text: node => (node.literal!),
   softbreak: () => ' ',
   linebreak: () => h.newline(),
   emph: node => h.italic(...transformChildren(node)),
   strong: node => h.bold(...transformChildren(node)),
-  html_inline: node => esacpeSlot(node.literal!),
+  html_inline: node => (node.literal!),
   link: node => h.link(stripNulls({ href: node.destination!, title: node.title }), ...transformChildren(node)),
   image: node => h.image(stripNulls({ src: node.destination!, title: node.title }), ...transformChildren(node)),
-  code: node => h.code(esacpeSlot(node.literal!)),
+  code: node => h.code((node.literal!)),
   document: node => h.template(...transformChildren(node)),
   paragraph: node => h.paragraph(...transformChildren(node)),
   block_quote: node => h.blockquote(...transformChildren(node)),
   item: node => h.item(...transformChildren(node)),
   list: node => h.list({ ordered: node.listType === 'ordered' }, ...transformChildren(node)),
   heading: node => h.heading({ level: node.level }, ...transformChildren(node)),
-  code_block: node => h.codeblock(stripNulls({ info: node.info }), esacpeSlot(node.literal!)),
-  html_block: node => esacpeSlot(node.literal!),
+  code_block: node => h.codeblock(stripNulls({ info: node.info }), node.literal!),
+  html_block: node => node.literal!,
   thematic_break: () => h.divider(),
   custom_inline: () => { throw new Error(`Function custom_inline is not implemented.`) },
   custom_block: () => { throw new Error(`Function custom_block is not implemented.`) },
@@ -66,48 +81,7 @@ function transformNode(node: Node): Fragment {
 
 function transformChildren(node: Node): Fragment[] {
   const children: Fragment[] = []
-  for (let child = node.firstChild; child; child = child.next) {
-    const node = transformNode(child)
-    if (node === '<slot>') {
-      const index = +(child = child.next!).literal!
-      children.push(slotValues[index]!)
-      child = child.next!
-    }
-    else {
-      children.push(node)
-    }
-  }
-  if (children.length === 1 && typeof children[0] === 'string') {
-    const fragment = esacpeSlot(children[0])
-    if (fragment instanceof Element) {
-      return fragment.children
-    }
-    return [fragment]
-  }
+  for (let child = node.firstChild; child; child = child.next)
+    children.push(transformNode(child))
   return children
-}
-
-function esacpeSlot(...children: [Fragment]): Fragment {
-  while (typeof children[children.length - 1] === 'string') {
-    const lastStr = children[children.length - 1] as string
-
-    const slotRegex = /<slot>(\d+)<\/slot>/
-    const match = lastStr.match(slotRegex)
-
-    if (!match) {
-      break
-    }
-
-    children.pop()
-
-    const before = lastStr.slice(0, match.index)
-    const value = slotValues[+match[1]!]!
-    const after = lastStr.slice(match.index! + match[0].length)
-
-    before && children.push(before)
-    children.push(value)
-    after && children.push(after)
-  }
-
-  return pack(...children)
 }
--- packages/element/utils.ts
+++ packages/element/utils.ts
@@ -3,24 +3,82 @@ import h, { Element } from './jsx-runtime'
 
 export { markdown } from './markdown'
 
-export function pack(...children: Fragment[]): Fragment {
-  return children.length === 1 ? children[0]! : h.template(...children)
+export function pack(children: Fragment[]): Fragment {
+  return children.length === 1 ? children[0]! : h.template(...children.flatMap(unpack))
+}
+
+export function unpack(fragment: Fragment): Fragment[] {
+  if (fragment instanceof Element && fragment.type === h.Fragment)
+    return fragment.children.flatMap(unpack)
+  return [fragment]
 }
 
 export function raw(strings: TemplateStringsArray, ...values: Fragment[]): Fragment {
-  return pack(...strings.flatMap((s, i) => values[i] ? [s, values[i]] : [s]))
+  return pack(strings.flatMap((s, i) => values[i] ? [s, values[i]] : [s]))
 }
-
-type Transformer = (element: Element) => Fragment
 
 export function transform(
   fragment: Fragment,
-  visitors: Partial<Record<keyof JSX.IntrinsicElements, Transformer>>,
+  visitors:
+    & { text?: (text: string) => Iterable<Fragment> }
+    & { [K in keyof JSX.IntrinsicElements]?: (element: Element<K>) => Iterable<Fragment> },
 ): Fragment {
-  if (fragment instanceof Element) {
-    visitors[fragment.type]?.(fragment)
+  if (typeof fragment === 'string' && visitors.text) {
+    fragment = pack(Array.from(visitors.text(fragment)))
+  }
+  else if (fragment instanceof Element) {
     fragment.children = fragment.children
-      .map(child => transform(child, visitors))
+      .flatMap(child => unpack(transform(child, visitors)))
+    const visit = visitors[fragment.type]
+    if (visit)
+      fragment = pack(Array.from(visit(fragment as any)))
   }
   return fragment
+}
+
+const lazy = (inner: any) => () => inner
+
+export function* replace(
+  string: string,
+  pattern: string | RegExp,
+  replacer: Fragment | ((substring: string, ...args: any[]) => Fragment),
+): Generator<Fragment> {
+  if (typeof pattern === 'string')
+    pattern = new RegExp(RegExp.escape(pattern))
+  if (typeof replacer !== 'function')
+    replacer = lazy(replacer)
+
+  if (!pattern.global) {
+    const match = pattern.exec(string)
+    if (match) {
+      const index = match.index
+      if (index > 0)
+        yield string.substring(0, index)
+      yield replacer(match[0], ...match.slice(1), index, string)
+      const after = index + match[0].length
+      if (after < string.length)
+        yield string.substring(after)
+    }
+    else {
+      yield string
+    }
+    return
+  }
+
+  let match: RegExpExecArray | null
+  let lastIndex = 0
+  pattern.lastIndex = 0
+  // eslint-disable-next-line no-cond-assign
+  while ((match = pattern.exec(string)) !== null) {
+    const matched = match[0]
+    const index = match.index
+    if (index > lastIndex)
+      yield string.substring(lastIndex, index)
+    yield replacer(match[0], ...match.slice(1), index, string)
+    lastIndex = index + matched.length
+    if (matched.length === 0 && pattern.lastIndex === index)
+      pattern.lastIndex++
+  }
+  if (lastIndex < string.length)
+    yield string.substring(lastIndex)
 }
