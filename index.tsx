29e3ce10534d10f04edba6c537129db7ebdcccfe
montmorill <montmorill@outlook.com>
2026-08-04 23:26:39 +0800

    feat: update console.log to use inspect for improved output formatting

--- index.tsx
+++ index.tsx
@@ -1,14 +1,20 @@
 /* eslint-disable no-console */
 
+import util, { inspect } from 'node:util'
 import h from '@yarkjs/element'
+
+// try uncomment this line!
+// util.inspect.defaultOptions.compact = false
+util.inspect.defaultOptions.colors = true
+util.inspect.defaultOptions.depth = Infinity
 
 const satori = <a href="https://satori.chat">Satori</a>
 const koishi = <a href="https://koishi.chat">Koishi</a>
 
-console.log(h.markdown`
+console.log(inspect(h.markdown`
 # Hello, ${<mention everyone />}!
 
 - I'm ${h.code('yark')},
 - created by ${<mention user="montmorill" />},
 - inspired by ${satori} & ${koishi}.
-`)
+`.toJSON()))
