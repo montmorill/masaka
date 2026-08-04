a7730ecf20d8d2f5cf75aa4ee5e39e2c60432720
montmorill <montmorill@outlook.com>
2026-08-04 16:42:22 +0800

    chore: remove redundant console.log statements for cleaner code

--- index.tsx
+++ index.tsx
@@ -5,9 +5,6 @@ import h from '@yarkjs/element'
 const satori = <a href="https://satori.chat">Satori</a>
 const koishi = <a href="https://koishi.chat">Koishi</a>
 
-console.log(h.h1('Hello, World!'))
-console.log(<h1>Hello, World!</h1>)
-
 console.log(h.markdown`
 # Hello, ${<mention everyone />}!
 
