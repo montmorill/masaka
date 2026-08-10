791346166c2a56876e8cf76436fa4c2edf134233
montmorill <montmorill@outlook.com>
2026-08-10 23:25:37 +0800

    feat(opcode): enhance OpCode enum with detailed comments and implement toString function

--- adapter/qq/index.ts
+++ adapter/qq/index.ts
@@ -1,7 +1,7 @@
-/* eslint-disable no-console */
 /* eslint-disable antfu/no-top-level-await */
 
 import { env } from 'node:process'
+import { logger } from '@yarkjs/logger'
 import QQBot from './bot'
 import * as QQ from './types'
 import QQBotWS from './ws'
@@ -10,7 +10,6 @@ if (!env.QQ_APP_ID || !env.QQ_SECRET)
   throw new Error('QQ_APP_ID or QQ_SECRET is not provided')
 
 const bot = await QQBot.create(env.QQ_APP_ID, env.QQ_SECRET)
-console.log('bot connected')
+logger.info('bot connected', bot.appId)
 const ws = await QQBotWS.create(bot, QQ.Intents.ALL)
-console.log('websocket connected')
-ws.on('*', console.log)
+logger.info('websocket connected', ws.sessionId)
