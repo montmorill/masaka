1c44743317fad23ee4d60db942b29d99979e0911
montmorill <montmorill@outlook.com>
2026-08-12 22:43:16 +0800

    refactor(formatter): enhance formatting options and improve styling logic

--- packages/element/formatter.ts
+++ packages/element/formatter.ts
@@ -1,10 +1,11 @@
 import type { InspectColor, InspectOptions } from 'node:util'
 import type { ElementJSON } from './jsx-runtime'
 import util from 'node:util'
-import { highlight } from '@babel/code-frame'
 import { Element, Fragment } from './jsx-runtime'
 
-export type FormatOptions = Pick<InspectOptions, 'colors' | 'compact'>
+export interface FormatOptions extends Pick<InspectOptions, 'colors' | 'compact'> {
+  highlight?: (code: string) => string
+}
 
 export class Formatter {
   private needLine = false
@@ -20,6 +21,12 @@ export class Formatter {
     }
   }
 
+  get compact(): boolean {
+    return typeof this.opts.compact === 'undefined' ? true
+      : typeof this.opts.compact === 'boolean' ? this.opts.compact
+        : this.depth > this.opts.compact
+  }
+
   nest(): Formatter {
     const formatter = new Formatter(this.print, this.opts)
     formatter.depth = this.depth + 1
@@ -27,18 +34,18 @@ export class Formatter {
   }
 
   newLine(): void {
-    if (this.opts.compact)
+    if (this.compact)
       return
     this.print(`\n${'  '.repeat(this.depth)}`)
     this.needLine = false
   }
 
-  styled(format: InspectColor, data: any): string {
-    return this.opts.colors ? util.styleText(format, data) : data
+  styled(type: keyof typeof Formatter.styles, data: any): string {
+    return this.opts.colors ? util.styleText(Formatter.styles[type], data) : data
   }
 
   string(value: string): void {
-    this.print(this.styled('green', JSON.stringify(value)))
+    this.print(this.styled('string', JSON.stringify(value)))
   }
 
   indented(value: string): void {
@@ -55,12 +62,13 @@ export class Formatter {
     const string = typeof object === 'undefined' ? 'undefined'
       : typeof object === 'function' ? object.toString()
         : JSON.stringify(object)
-    this.indented(`{${this.opts.colors ? highlight(string) : string}}`)
+    this.indented(`{${this.opts.colors && this.opts.highlight
+      ? this.opts.highlight(string) : string}}`)
   }
 
   attrs(attrs: Record<string, any>): void {
     for (const [key, value] of Object.entries(attrs)) {
-      this.print(` ${this.styled('red', key)}`)
+      this.print(` ${this.styled('attr', key)}`)
       if (value === true)
         continue
       this.print('=')
@@ -72,7 +80,7 @@ export class Formatter {
   }
 
   element(element: ElementJSON): void {
-    const tag = this.styled('yellow', element.type === Fragment ? '' : element.type)
+    const tag = this.styled('tag', element.type === Fragment ? '' : element.type)
     if (this.needLine)
       this.newLine()
     this.print(`<${tag}`)
@@ -109,6 +117,14 @@ export class Formatter {
     this.object(node)
     this.needLine = true
   }
+}
+
+export namespace Formatter {
+  export const styles = {
+    string: 'green',
+    attr: 'red',
+    tag: 'yellow',
+  } as const satisfies Record<string, InspectColor>
 }
 
 export class BufferFormatter extends Formatter {
