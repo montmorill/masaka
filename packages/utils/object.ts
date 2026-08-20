5f3d312755624af5dd47ebf739ee7ef64dead449
montmorill <montmorill@outlook.com>
2026-08-20 20:42:23 +0800

    refactor(object): rename filterKeys to filterEntries for consistency

--- packages/utils/object.ts
+++ packages/utils/object.ts
@@ -16,22 +16,6 @@ export function mapKeys<
   return res
 }
 
-export function filterKeys<
-  K extends { [P in Extract<keyof T, string>]: boolean },
-  T extends Record<string, unknown>,
-  F extends <P extends Extract<keyof T, string>>(key: P, value: T[P]) => boolean,
->(
-  obj: T,
-  predicate: F,
-): { [P in Extract<keyof T, string> as K[P] extends false ? never : P]: T[P] } {
-  const res = {} as any
-  for (const key in obj) {
-    if (predicate(key, obj[key]))
-      res[key] = obj[key]
-  }
-  return res
-}
-
 export function withPrefix<
   P extends string,
   T extends Record<string, any>,
@@ -51,7 +35,23 @@ export function snakeCaseKeys<T extends Record<string, any>>(obj: T): SnakeCaseK
   return mapKeys(obj, key => cases.snake(splitWords(key))) as any
 }
 
+export function filterEntries<
+  K extends { [P in Extract<keyof T, string>]: boolean },
+  T extends Record<string, unknown>,
+  F extends <P extends Extract<keyof T, string>>(key: P, value: T[P]) => boolean,
+>(
+  obj: T,
+  predicate: F,
+): { [P in Extract<keyof T, string> as K[P] extends false ? never : P]: T[P] } {
+  const res = {} as any
+  for (const key in obj) {
+    if (predicate(key, obj[key]))
+      res[key] = obj[key]
+  }
+  return res
+}
+
 export function stripNullish<T extends Record<string, any>>(object: T):
 Pretty<Overwrite<T, { [K in keyof T as null extends T[K] ? K : never]?: NonNullable<T[K]> }>> {
-  return filterKeys(object, (_, value) => value != null) as any
+  return filterEntries(object, (_, value) => value != null) as any
 }
